7adc0afc204d9c272e7c65af06ccf537740b0dc7
felipe-boneti <felipe_gustavo_boneti@outlook.com>
2023-06-02 22:28:19 -0300

    Atualização na pasta modelo do professor

--- Arquivos temporários/modelo/conexao.php
+++ Arquivos temporários/modelo/conexao.php
@@ -1,11 +1,8 @@
 <?php
-	header('Content-type: text/html; charset=utf-8');
-
 	$servidor="localhost";
 	$banco="dbaula";
 	$usuario="root";
 	$senha="";
 
-	$pdo = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	
+	$pdo = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha);		
 ?>
